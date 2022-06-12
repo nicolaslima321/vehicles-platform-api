8c702bddc8bcc52abe3c210cf880e95aeae003a5
Nicolas Lima <nicolas.pedroso2145@gmail.com>
2022-06-12 10:23:30 -0300

    created database connection

---
+++ config/database.js
@@ -0,0 +1,18 @@
+import { Sequelize } from "sequelize";
+
+const dbName = process.env.DB_NAME;
+const dbUser = process.env.DB_USERNAME;
+const dbHost = process.env.DB_HOST;
+const dbType = process.env.DB_TYPE;
+const dbPassword = process.env.DB_PASSWORD;
+
+const dbConfigs = {
+  define: {
+    timestamps: false,
+    underscored: true,
+  },
+  dialect: dbType,
+  host: dbHost,
+};
+
+export default new Sequelize(dbName, dbUser, dbPassword, dbConfigs);
